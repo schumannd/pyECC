77bdc7140e483a56f71d21a6ceceaf9da1d54efc
schumannd <dnstra@gmail.com>
2013-11-14 13:15:32 -0500

    current state of project, basic ecc functions are implemented

---
+++ Point.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+class Point:
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
---
+++ ecc.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+import sys, os
+from Point import Point
+
+def addEC(p1, p2, a, p):
+	if p1.x == p2.x and p1.y == p2.y:
+		m = (3*p1.x*p1.x + a)*modularInverse(2*p1.y, p) % p
+		x3 = (m*m - 2*p1.x) % p
+		y3 = -(p1.y + m*(x3 - p1.x)) % p
+	elif p1.x == p2.x and p1.y != p2.y:
+		raise Exception("reached Infinity")
+	elif p1.x != p2.x and p1.y != p2.y:
+		m = (p2.y -p1.y)*modularInverse(p2.x-p1.x, p) % p
+		x3 = (m*m - (p1.x + p2.x)) % p
+		y3 = -(p2.y + m*(x3 - p2.x)) % p
+	return Point(x3,y3)
+
+def mulEC(Point, factor, a, p):
+	if factor == 1:
+		solution = Point
+	elif factor % 2 == 0:
+		halfPoint = mulEC(Point, factor/2, a, p)
+		solution = addEC(halfPoint, halfPoint, a, p)
+	else:
+		halfPoint = mulEC(Point, factor-1, a, p)
+		solution = addEC(halfPoint, Point, a, p)
+	return Point
+	
+def decrypt(Y, Alpha, privateN, a, p):
+	negativePoint = mulEC(Alpha, privateN, a, p)
+	negativePoint.y = negativePoint.y*-1
+	return addEC(negativePoint, Y, a, p)
+
+def bruteForce(p, a, b, G, P):
+	current = Point(G.x, G.y)
+	N = 1
+	while current.x != P.x or current.y != P.y:
+		current = addEC(current, G, a, p)
+		N +=1
+		if N%1000000 == 0:
+			print N
+	return N
+
+def pulverizer(a, b): # a > b
+    x1, y1, x2, y2 = 1, 0, 0, 1
+    while b != 0:
+    	q, r = a//b, a%b
+    	x, y = x1 - q*x2, y1 - q*y2
+    	a, b, x1, y1, x2, y2 = b, r, x2, y2, x, y
+    	# print str(q)+", "+str(r)+", "+str(a)+", "+str(b)+", "+str(x1)+", "+str(y1)+", "+str(x2)+", "+str(y2)
+    return a, x1, y1
+
+def modularInverse(e, phi):
+    g, x, y = pulverizer(e, phi)
+    if g != 1:
+        raise Exception('modular inverse does not exist')
+    else:
+        return x % phi
